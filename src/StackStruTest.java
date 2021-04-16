69bcd408d659fc61aa1cf6a97d9058ceb7496ad7
echoRookie <echorookie@gmail.com>
2021-04-16 13:39:36 +0800

    feat:JVM class文件反编译

---
+++ src/StackStruTest.java
@@ -0,0 +1,14 @@
+/**
+ * @author yangjun6
+ * @date 2021-04-15 22:51
+ */
+public class StackStruTest {
+    public static void main(String[] args) {
+        int i = 2 + 3;
+        try {
+            Thread.sleep(8000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
