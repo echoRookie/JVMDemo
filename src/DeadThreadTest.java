b9c3c81c9d9a2c5e20d5ed73fd2ea37942108bfd
echoRookie <echorookie@gmail.com>
2021-04-17 18:38:37 +0800

    feat:JVM 类加载器

---
+++ src/DeadThreadTest.java
@@ -0,0 +1,27 @@
+/**
+ * @author yangjun6
+ * @date 2021-04-17 18:36
+ */
+public class DeadThreadTest {
+    public static void main(String[] args) {
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + "\t 线程t1开始");
+            new DeadThread();
+        }, "t1").start();
+
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + "\t 线程t2开始");
+            new DeadThread();
+        }, "t2").start();
+    }
+}
+class DeadThread {
+    static {
+        if (true) {
+            System.out.println(Thread.currentThread().getName() + "\t 初始化当前类");
+            while(true) {
+
+            }
+        }
+    }
+}
---
+++ src/HelloLoader.java
@@ -0,0 +1,10 @@
+/**
+ * @author yangjun6
+ * @date 2021-04-17 10:48
+ * 验证类加载器
+ */
+public class HelloLoader {
+    public static void main(String[] args) {
+        System.out.println("我已经被加载");
+    }
+}
