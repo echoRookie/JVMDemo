96b31221fa35cc6536ff5642bcfffa590ae5b92b
echoRookie <echorookie@gmail.com>
2021-04-20 13:55:43 +0800

    feat:JVM双亲委派机制

---
+++ src/java/lang/Test.java
@@ -0,0 +1,11 @@
+package java.lang;
+
+/**
+ * @author yangjun6
+ * @date 2021-04-19 22:14
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
