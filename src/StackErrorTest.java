fe4fbe217279bb563495a58699a2931943a7070c
echoRookie <echorookie@gmail.com>
2021-04-22 10:57:38 +0800

    feat:JVM虚拟机栈局部变量表

--- src/StackErrorTest.java
+++ src/StackErrorTest.java
@@ -8,4 +8,13 @@ public class StackErrorTest {
         System.out.println("test");
         main(args);
     }
+    public void test(){
+        int a = 0;
+        {
+            int b = 1;
+            b = a + 1;
+        }
+        // 变量c使用已经被销毁的b的位置，所以局部变量表的长度还是3
+        int c = a + 1;
+    }
 }
