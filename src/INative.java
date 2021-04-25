0a45ac8a56dd4158e7f9e616ef980e0771fa2c26
echoRookie <echorookie@gmail.com>
2021-04-25 23:14:22 +0800

    feat:JVM本地方法接口

---
+++ src/INative.java
@@ -0,0 +1,9 @@
+/**
+ * @author yangjun6
+ * @date 2021-04-25 23:12
+ * 测试定义本地方法
+ */
+public class INative {
+    native int test();
+}
+
---
+++ src/OperandStackTest.java
@@ -0,0 +1,11 @@
+/**
+ * @author yangjun6
+ * @date 2021-04-22 22:13
+ */
+public class OperandStackTest {
+    public void test(){
+        byte i = 15;
+        int j = 8;
+        int k = i+j;
+    }
+}
